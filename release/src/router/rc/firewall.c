fbd807d074a4494d0320a1ff7e23385482f71b77
Keith Moyer <keith@keith-laptop.keithmoyer.com>
2008-12-02 12:41:57 -0600

    Allow domain name as admin access restriction

--- release/src/router/rc/firewall.c
+++ release/src/router/rc/firewall.c
@@ -114,6 +114,15 @@ static void ipt_source(const char *s, char *src)
 {
 	if ((*s) && (strlen(s) < 32)) sprintf(src, "-%s %s", strchr(s, '-') ? "m iprange --src-range" : "s", s);
 		else *src = 0;
+	if ((*s) && (strlen(s) < 32))
+	{
+		if (sscanf(s,"%*d.%*d.%*d.%*d-%*d.%*d.%*d.%*d") == 8)
+			sprintf(src, "-m iprange --src-range %s", s);
+		else
+			sprintf(src, "-s %s", s);
+	}
+	else
+		*src = 0;
 }
 
 /*
@@ -121,8 +130,11 @@ static void get_src(const char *nv, char *src)
 {
 	char *p;
 
-	if (((p = nvram_get(nv)) != NULL) && (*p) && (strlen(p) < 32)) {
-		sprintf(src, "-%s %s", strchr(p, '-') ? "m iprange --src-range" : "s", p);
+	if (((p=nvram_get(nv)) != NULL) && (*p) && (strlen(p) < 32)) {
+		if (sscanf(p,"%*d.%*d.%*d.%*d-%*d.%*d.%*d.%*d") == 8)
+			sprintf(src, "-m iprange --src-range %s", p);
+		else
+			sprintf(src, "-s %s", p);
 	}
 	else {
 		*src = 0;
