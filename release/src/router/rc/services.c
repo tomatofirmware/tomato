22242cb9b72da66e696140688f0857c63e2adcdb
Fedor Kozhevnikov <fedork@ubuntu.(none)>
2011-04-04 16:44:18 -0400

    rc: restart usb services (Samba/FTP/Media Server) when network is restarted
    This is ok since the LAN/WAN communications are interrupted during the net
    restart anyway. This fixes MiniDLNA getting confused with no longer valid
    binding.

--- release/src/router/rc/services.c
+++ release/src/router/rc/services.c
@@ -2176,6 +2176,9 @@ TOP:
 
 	if (strcmp(service, "net") == 0) {
 		if (action & A_STOP) {
+#ifdef TCONFIG_USB
+			stop_nas_services();
+#endif
 #ifdef TCONFIG_IPV6
 			stop_radvd();
 #endif
@@ -2197,6 +2200,9 @@ TOP:
 			start_radvd();
 #endif
 			start_wl();
+#ifdef TCONFIG_USB
+			start_nas_services();
+#endif
 		}
 		goto CLEAR;
 	}
