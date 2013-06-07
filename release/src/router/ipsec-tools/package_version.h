18972c1662410c5f1953797f4a72d4f4e784cdc6
Kevin Darbyshire-Bryant <kdbtomato@darbyshire-bryant.me.uk>
2013-06-07 12:06:15 +0100

    IPSEC l2tp vpn - from Daniel Borca - compiles now

---
+++ release/src/router/ipsec-tools/package_version.h
@@ -0,0 +1,5 @@
+#define TOP_PACKAGE "ipsec-tools"
+#define TOP_PACKAGE_NAME "ipsec-tools"
+#define TOP_PACKAGE_VERSION "0.8.1"
+#define TOP_PACKAGE_STRING "ipsec-tools 0.8.1"
+#define TOP_PACKAGE_URL "http://ipsec-tools.sourceforge.net"
