dcadcaa770839a96c1e13af9ca30d7c2a536fb71
Vicente <victek@tomatoraf.com>
2013-06-09 17:14:18 +0200

    Clean and tiddy-up files.

---
+++ release/src/lzma/C/Compress/Lzma/.LzmaDecode.o.cmd
@@ -0,0 +1,11 @@
+cmd_fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.o := mipsel-uclibc-gcc -Wp,-MD,fs/squashfs/../../../../lzma/C/Compress/Lzma/.LzmaDecode.o.d  -nostdinc -isystem /home/vicente/tomato/tools/brcm/K26/hndtools-mipsel-uclibc-4.2.4/bin/../lib/gcc/mipsel-linux-uclibc/4.2.4/include -D__KERNEL__ -Iinclude  -include include/linux/autoconf.h -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -fno-delete-null-pointer-checks -I/home/vicente/tomato/release/src-rt/include -DBCMDRIVER -Dlinux -O2 -ffunction-sections  -mno-check-zero-division -mabi=32 -G 0 -mno-abicalls -fno-pic -pipe -msoft-float -ggdb -ffreestanding  -march=mips32r2 -Wa,-mips32r2 -Wa,--trap  -Iinclude/asm-mips/mach-generic -fomit-frame-pointer  -fno-stack-protector -funit-at-a-time -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow  -DHNDCTF -DCTFPOOL -DCTFMAP -Ifs/squashfs/../../../../lzma/C/Compress/Lzma   -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(LzmaDecode)"  -D"KBUILD_MODNAME=KBUILD_STR(squashfs)" -c -o fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.o fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.c
+
+deps_fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.o := \
+  fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.c \
+  fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.h \
+  fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaTypes.h \
+  /home/vicente/tomato/tools/brcm/K26/hndtools-mipsel-uclibc-4.2.4/bin/../lib/gcc/mipsel-linux-uclibc/4.2.4/include/stddef.h \
+
+fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.o: $(deps_fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.o)
+
+$(deps_fs/squashfs/../../../../lzma/C/Compress/Lzma/LzmaDecode.o):
