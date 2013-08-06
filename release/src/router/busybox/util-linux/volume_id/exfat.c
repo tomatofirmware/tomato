87a40a1775f5267eb6dc2937abd641d302abf739
Vicente <Victek@tomatoraf.com>
2013-08-06 14:44:58 +0200

    Busybox: Upgrade to 1.21.1 (stable). lsof active.

--- release/src/router/busybox/util-linux/volume_id/exfat.c
+++ release/src/router/busybox/util-linux/volume_id/exfat.c
@@ -16,7 +16,6 @@
  *	You should have received a copy of the GNU Lesser General Public
  *	License along with this library; if not, write to the Free Software
  *	Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
- *	From Busybox 1.21.1 to Tomato RAF
  */
 
 #include "volume_id_internal.h"
