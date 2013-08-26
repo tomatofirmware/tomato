024f036cacf8650e3167f6f79e1dba6ef9898fda
Zhiqiang Wang <wang.9264@gmail.com>
2013-08-26 14:35:35 -0400

    1. change jffs2 size calc method. it should be a bug while set it's size. it was set as size= PART_JFFS2_MIN * mtd->erasesize 2. add a PART_JFFS2_GAP build CFLAG to target.mak. PART_JFFS2_GAP was used in linux/linux-2.6/arch/mips/brcm-boards/bcm947xx/nonand-setup.c    and defined as 5 blocks (256k). lots of current router has flash more than 16Mb. the 256k limit is just too small.    by adding this build option. we can set it's value in Makefile. or just set "0" to use all unused space. 3. some mksquashfs use squashfs-lzma instead of squashfs for rootfs. we correct some detect method in router/rc and router/httpd
    Change-Id: I8a5897b2e26ddea8b9b633f49fa9b336070b4fab

--- release/src-rt-6.x/linux/linux-2.6/arch/mips/brcm-boards/bcm947xx/setup-nonand.c
+++ release/src-rt-6.x/linux/linux-2.6/arch/mips/brcm-boards/bcm947xx/setup-nonand.c
@@ -339,7 +339,9 @@ enum {
 };
 
 #define PART_JFFS2_MIN 5
+#ifndef PART_JFFS2_GAP
 #define PART_JFFS2_GAP 0x40000UL /* 256K, power of 2 */
+#endif
 
 struct mtd_partition *
 init_mtd_partitions(struct mtd_info *mtd, size_t size)
@@ -393,10 +395,12 @@ init_mtd_partitions(struct mtd_info *mtd, size_t size)
 				bcm947xx_parts[PART_JFFS2].size = boardoff - off - trxsize;
 			len = PART_JFFS2_MIN * mtd->erasesize;
 			if (bcm947xx_parts[PART_JFFS2].size >= len) {
-#if 0 /* Gap jffs2 from rootfs up to 256K */
-				bcm947xx_parts[PART_JFFS2].size -= len;
-				bcm947xx_parts[PART_JFFS2].size &= ~(PART_JFFS2_GAP - 1); // round down
-				bcm947xx_parts[PART_JFFS2].size += len;
+#if 1 /* Gap jffs2 from rootfs up to 256K */
+				if(PART_JFFS2_GAP > 0) {
+					bcm947xx_parts[PART_JFFS2].size -= len;
+					bcm947xx_parts[PART_JFFS2].size &= ~(PART_JFFS2_GAP - 1); // round down
+					bcm947xx_parts[PART_JFFS2].size += len;
+				}
 #else /* Temporary compatibility for Boyau */
 				bcm947xx_parts[PART_JFFS2].size = len;
 #endif
