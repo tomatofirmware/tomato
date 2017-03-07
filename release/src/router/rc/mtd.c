6dcbb3a4e0151aa33f47124b4122a551bec6eed4
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2017-03-07 08:57:01 +0100

    Fix JFFS2 formatting for non RT-AC
    Reapply 5167d12f7bdb964af731c27586234ab07261945f to all builds
    Remove old unused code

--- release/src/router/rc/mtd.c
+++ release/src/router/rc/mtd.c
@@ -153,29 +153,21 @@ static int _unlock_erase(const char *mtdname, int erase)
 	int mf;
 	mtd_info_t mi;
 	erase_info_t ei;
-#ifdef CONFIG_BCMWL6
 	int r, ret, skipbb;
-#else
-	int r;
-#endif
 
 	if (!wait_action_idle(5)) return 0;
 	set_action(ACT_ERASE_NVRAM);
 	if (erase) led(LED_DIAG, 1);
 
 	r = 0;
-#ifdef CONFIG_BCMWL6
 	skipbb = 0;
-#endif
 	if ((mf = mtd_open(mtdname, &mi)) >= 0) {
 			r = 1;
-#if 1
 			ei.length = mi.erasesize;
 			for (ei.start = 0; ei.start < mi.size; ei.start += mi.erasesize) {
 				printf("%sing 0x%x - 0x%x\n", erase ? "Eras" : "Unlock", ei.start, (ei.start + ei.length) - 1);
 				fflush(stdout);
 
-#ifdef CONFIG_BCMWL6
 				if (!skipbb) {
 					loff_t offset = ei.start;
 
@@ -192,7 +184,6 @@ static int _unlock_erase(const char *mtdname, int erase)
 						}
 					}
 				}
-#endif
 				if (ioctl(mf, MEMUNLOCK, &ei) != 0) {
 //					perror("MEMUNLOCK");
 //					r = 0;
@@ -206,24 +197,6 @@ static int _unlock_erase(const char *mtdname, int erase)
 					}
 				}
 			}
-#else
-			ei.start = 0;
-			ei.length = mi.size;
-
-			printf("%sing 0x%x - 0x%x\n", erase ? "Eras" : "Unlock", ei.start, ei.length - 1);
-			fflush(stdout);
-
-			if (ioctl(mf, MEMUNLOCK, &ei) != 0) {
-				perror("MEMUNLOCK");
-				r = 0;
-			}
-			else if (erase) {
-				if (ioctl(mf, MEMERASE, &ei) != 0) {
-					perror("MEMERASE");
-					r = 0;
-				}
-			}
-#endif
 
 			// checkme:
 			char buf[2];
